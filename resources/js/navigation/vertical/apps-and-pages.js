53a5719b893c17a316ea62c3e5ed7a5d3769cace
majid <ghamajid135220@gmail.com>
2023-12-25 17:33:39 +0330

    Add project

---
+++ resources/js/navigation/vertical/apps-and-pages.js
@@ -0,0 +1,18 @@
+export default [
+  { heading: 'Apps & Pages' },
+  {
+    title: 'Users',
+    icon: { icon: 'tabler-users' },
+    to: 'apps-user-list',
+  },
+  {
+    title: 'Roles',
+    icon: { icon: 'tabler-settings' },
+    to: 'apps-roles',
+  },
+  {
+    title: 'Permissions',
+    icon: { icon: 'tabler-settings' },
+    to: 'apps-permissions',
+  },
+]
